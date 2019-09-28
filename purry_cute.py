531c995e85935f4ffdae6e16ff57f81244be7062
Michał Bartyński <bartynski.michal@gmail.com>
2019-09-28 14:47:37 +0200

    now penguins are draw in correct order

--- purry_cute.py
+++ purry_cute.py
@@ -17,7 +17,7 @@ def get_image(path):
 
 
 BG_IMGS = [get_image("img/background.png"), get_image("img/distant_trees.png"),
-           get_image("img/trees_and_bushes.png"), get_image("img/ground.png")]
+           get_image("img/trees_and_bushes_1.png"), get_image("img/ground.png")]
 
 PENGUIN_IMGS = []
 
@@ -31,6 +31,7 @@ class Background:
     VEL_DISTANT_TREES = 1
     WIDTH = BG_IMGS[0].get_width()
     IMG = BG_IMGS
+    TREE_HEIGHT = IMG[0].get_height() - IMG[2].get_height()
 
     def __init__(self, y=0):
         self.y = y
@@ -40,6 +41,9 @@ class Background:
         self.trees_x2 = self.WIDTH
         self.dist_trees_x1 = 0
         self.dist_trees_x2 = self.WIDTH
+
+    def get_y(self):
+        return self.y
 
     def move(self):
         self.ground_x1 -= self.VEL_GROUND
@@ -67,23 +71,18 @@ class Background:
         if self.dist_trees_x2 + self.WIDTH < 0:
             self.dist_trees_x2 = self.dist_trees_x1 + self.WIDTH
 
-    def draw(self, win, penguin, enemies):
+    def draw(self, win, list_of_objects):
+        win.blit(self.IMG[0], (0, 0))
         win.blit(self.IMG[1], (self.dist_trees_x1, 0))
         win.blit(self.IMG[1], (self.dist_trees_x2, 0))
-
-        if penguin.get_y() < (583 - penguin.get_height()):
-            penguin.draw()
-
-        win.blit(self.IMG[2], (self.trees_x1, 0))
-        win.blit(self.IMG[2], (self.trees_x2, 0))
+        win.blit(self.IMG[2], (self.trees_x1, self.TREE_HEIGHT))
+        win.blit(self.IMG[2], (self.trees_x2, self.TREE_HEIGHT))
         win.blit(self.IMG[3], (self.ground_x1, 90))
         win.blit(self.IMG[3], (self.ground_x2, 90))
 
-        if penguin.get_y() > (583 - penguin.get_height()):
-            penguin.draw()
-
-        for enemy in enemies:
-            enemy.draw()
+        list_of_objects.sort(key=lambda x: x.y)
+        for obj in list_of_objects:
+            obj.draw()
 
 
 class Penguin:
@@ -118,25 +117,30 @@ class Penguin:
         return self.lives > 0
 
     def move(self, key):
-        self.moving_backward = False
-        if key[pygame.K_UP] and self.y > 490:
-            self.y -= 3
-        if key[pygame.K_DOWN] and self.y < 620:
-            self.y += 3
-        if key[pygame.K_LEFT] and self.x > 0:
-            self.x -= self.vel_backward
-            self.moving_backward = True
-        if key[pygame.K_RIGHT] and self.x < (1024 - self.WIDTH):
-            self.x += self.vel_forward
-
-    def enemy_move(self):
-        if self.x > 0:
-            self.x -= 5
+        if self.enemy:
+            if self.x > 0:
+                self.x -= 8
+        else:
+            self.moving_backward = False
+            if key[pygame.K_UP] and self.y > 490:
+                self.y -= 3
+            if key[pygame.K_DOWN] and self.y < 620:
+                self.y += 3
+            if key[pygame.K_LEFT] and self.x > 0:
+                self.x -= self.vel_backward
+                self.moving_backward = True
+            if key[pygame.K_RIGHT] and self.x < (1024 - self.WIDTH):
+                self.x += self.vel_forward
 
     def draw(self):
+        """
+        Determinate direction and draw character on a screen
+        :return:
+        """
         if self.img_count > 15:
             self.img_count = 0
 
+        # we want to know if character moves backward to reverse image
         if self.moving_backward or self.enemy:
             screen.blit(pygame.transform.flip(PENGUIN_IMGS[self.img_count], True, False), (self.x, self.y))
         else:
@@ -145,6 +149,11 @@ class Penguin:
         self.img_count += 1
 
     def get_mask(self):
+        """
+
+        :return: tuple of coordinates of object edges needed to determinate if hit
+        """
+        print(pygame.mask.from_surface(self.img))
         return pygame.mask.from_surface(self.img)
 
 
@@ -152,29 +161,27 @@ pygame.init()
 screen = pygame.display.set_mode((1024, 773))
 done = False
 bg = Background()
-penguin = Penguin(100, 620, 2)
-enemies = [Penguin(1200, 620, enemy=True)]
+player = Penguin(100, 620, 2)
+list_to_draw = [player, Penguin(1200, 620, enemy=True)]
 clock = pygame.time.Clock()
 
-while not done and penguin.is_alive():
+while not done and player.is_alive():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
 
     add_enemy = False
     rem = []
-
-    for i in range(len(enemies)):
-        enemies[i].enemy_move()
-        distance = random.randint(600, 950) - random.randint(0, 100)
-        if len(enemies) < 10 and enemies[len(enemies) - 1].get_x() < distance:
-            print(distance)
-            enemies.append(Penguin(1100, random.randint(520, 620), enemy=True))
-
     pressed = pygame.key.get_pressed()
-    penguin.move(pressed)
-    screen.blit(get_image("img/background.png"), (0, 0))
-    bg.draw(screen, penguin, enemies)
+    player.move(pressed)
+
+    for i in range(len(list_to_draw)):
+        list_to_draw[i].move(pressed)
+        distance = random.randint(600, 950) - random.randint(0, 100)
+        if len(list_to_draw) < 6 and list_to_draw[-1].get_x() < distance:
+            list_to_draw.append(Penguin(1100, random.randint(520, 620), enemy=True))
+
+    bg.draw(screen, list_to_draw)
     bg.move()
     pygame.display.flip()
     clock.tick(32)
