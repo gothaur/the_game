599995fcd23987ee56e17b49f3f141ed63ea16b6
Michał Bartyński <bartynski.michal@gmail.com>
2019-09-30 18:46:09 +0200

    fixed shooting and add background movment

--- penguin_of_war.py
+++ penguin_of_war.py
@@ -2,29 +2,30 @@ import pygame
 import os
 import random
 
-_image_library = {}
-
 
 def get_image(path):
-    global _image_library
-    image = _image_library.get(path)
+    """
+    Changes slash or backslash depends of the system you uses
+    :param path:
+    :return image with correct path:
+    """
+    canonicalized_path = path.replace('/', os.sep).replace('\\', os.sep)
+    return pygame.image.load(canonicalized_path)
 
-    if image is None:
-        canonicalized_path = path.replace('/', os.sep).replace('\\', os.sep)
-        image = pygame.image.load(canonicalized_path)
-        _image_library[path] = image
-    return image
 
-
+# list of images to draw a background
 BG_IMGS = [get_image("img/background.png"), get_image("img/distant_trees.png"),
            get_image("img/trees_and_bushes_1.png"), get_image("img/ground.png")]
 
 PROJECTILE_IMG = get_image("img/projectile.png")
 
+# list of penguin images required to make movement animation
 PENGUIN_IMGS = []
-
 for i in range(1, 17):
     PENGUIN_IMGS.append(get_image(f"img/penguin/Armature_run_{i}.png"))
+
+HEIGHT = BG_IMGS[0].get_height()
+WIDTH = BG_IMGS[0].get_width()
 
 
 class Background:
@@ -48,12 +49,28 @@ class Background:
         return self.y
 
     def move(self, player):
-        self.ground_x1 -= self.VEL_GROUND + player.get_speed()
-        self.ground_x2 -= self.VEL_GROUND + player.get_speed()
-        self.trees_x1 -= self.VEL_TREES_AND_BUSHES
-        self.trees_x2 -= self.VEL_TREES_AND_BUSHES
-        self.dist_trees_x1 -= self.VEL_DISTANT_TREES
-        self.dist_trees_x2 -= self.VEL_DISTANT_TREES
+        if player.is_moving:
+            if player.moving_backward:
+                self.ground_x1 -= (self.VEL_GROUND - player.get_vel())
+                self.ground_x2 -= (self.VEL_GROUND - player.get_vel())
+                self.trees_x1 -= self.VEL_TREES_AND_BUSHES - player.get_vel()
+                self.trees_x2 -= self.VEL_TREES_AND_BUSHES - player.get_vel()
+                self.dist_trees_x1 -= self.VEL_DISTANT_TREES - player.get_vel()
+                self.dist_trees_x2 -= self.VEL_DISTANT_TREES - player.get_vel()
+            else:
+                self.ground_x1 -= (self.VEL_GROUND + player.get_vel())
+                self.ground_x2 -= (self.VEL_GROUND + player.get_vel())
+                self.trees_x1 -= self.VEL_TREES_AND_BUSHES + player.get_vel()
+                self.trees_x2 -= self.VEL_TREES_AND_BUSHES + player.get_vel()
+                self.dist_trees_x1 -= self.VEL_DISTANT_TREES + player.get_vel()
+                self.dist_trees_x2 -= self.VEL_DISTANT_TREES + player.get_vel()
+        else:
+            self.ground_x1 -= self.VEL_GROUND
+            self.ground_x2 -= self.VEL_GROUND
+            self.trees_x1 -= self.VEL_TREES_AND_BUSHES
+            self.trees_x2 -= self.VEL_TREES_AND_BUSHES
+            self.dist_trees_x1 -= self.VEL_DISTANT_TREES
+            self.dist_trees_x2 -= self.VEL_DISTANT_TREES
 
         if self.ground_x1 + self.WIDTH < 0:
             self.ground_x1 = self.ground_x2 + self.WIDTH
@@ -82,6 +99,7 @@ class Background:
         win.blit(self.IMG[3], (self.ground_x1, 90))
         win.blit(self.IMG[3], (self.ground_x2, 90))
 
+        # list of penguins must be in correct order to draw
         list_of_objects.sort(key=lambda x: x.y)
         for obj in list_of_objects:
             obj.draw()
@@ -105,8 +123,9 @@ class Penguin:
         self.lives = lives
         self.moving_backward = False
         self.enemy = enemy
+        self.is_moving = False
 
-    def get_speed(self):
+    def get_vel(self):
         return self.vel_forward
 
     def get_y(self):
@@ -130,6 +149,7 @@ class Penguin:
                 self.x -= 8
         else:
             self.moving_backward = False
+            self.is_moving = False
             if key[pygame.K_UP] and self.y > 595 - self.HEIGHT:
                 self.y -= 3
             if key[pygame.K_DOWN] and self.y < 620:
@@ -137,7 +157,9 @@ class Penguin:
             if key[pygame.K_LEFT] and self.x > 0:
                 self.x -= self.vel_backward
                 self.moving_backward = True
+                self.is_moving = True
             if key[pygame.K_RIGHT] and self.x < (1024 - self.WIDTH):
+                self.is_moving = True
                 self.x += self.vel_forward
 
     def draw(self):
@@ -170,22 +192,20 @@ class Projectile:
     def __init__(self, penguin):
         self.x = penguin.get_x() + penguin.get_height()
         self.y = int(penguin.get_y() + penguin.get_width() / 2)
+        self.penguin = penguin
 
     def draw(self):
         screen.blit(self.IMG, (self.x, self.y))
 
     def move(self):
-        self.x += 6
+        self.x += self.penguin.get_vel() + 6
 
     def collide(self, penguin):
         penguin_mask = penguin.get_mask()
         projectile_mask = pygame.mask.from_surface(self.IMG)
 
         offset = (self.x - penguin.get_x(), self.y - penguin.get_y())
-        point = penguin_mask.overlap(projectile_mask, offset)
-        if point:
-            return True
-        return False
+        return penguin_mask.overlap(projectile_mask, offset)
 
 
 pygame.init()
@@ -201,34 +221,38 @@ while not done and player.is_alive():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            bullets.append(Projectile(player))
 
     add_enemy = False
     rem_p = []
     rem_b = []
+
     pressed = pygame.key.get_pressed()
     player.move(pressed)
 
-    if pressed[pygame.K_SPACE]:
-        bullets.append(Projectile(player))
-
     for bullet in bullets:
+        if bullet.x >= WIDTH:
+            rem_b.append(bullet)
+            continue
         for i in range(len(list_to_draw)):
             if bullet.collide(list_to_draw[i]):
                 rem_p.append(list_to_draw[i])
                 rem_b.append(bullet)
-                print(f"zawartosc listy rem_p {rem_p}, rem_b {rem_b}")
 
         bullet.move()
 
     for r in rem_p:
-        print(f"obiekt do usuniecia w rem_p {r}")
-        list_to_draw.remove(r)
-        print("usunieto z rem_p")
+        try:
+            list_to_draw.remove(r)
+        except Exception:
+            print("Attempt to remove non existing penguin")
 
     for r in rem_b:
-        print(f"obiekt do usuniecia w rem_b{r}")
-        bullets.remove(r)
-        print("usunieto z rem_b")
+        try:
+            bullets.remove(r)
+        except Exception:
+            print("Attempt to remove non existing bullet")
 
     for i in range(len(list_to_draw)):
         list_to_draw[i].move(pressed)
